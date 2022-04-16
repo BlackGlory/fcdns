ab2352ff55dd05f947fa8eedd9dc6706e36c67ad
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-04-17 00:15:48 +0800

    fix: make hostname list files optional

--- src/hostname-list.ts
+++ src/hostname-list.ts
@@ -1,10 +1,12 @@
 import { readHostnameListFile } from '@utils/hostname-list-file'
 import { HostnamePattern } from '@utils/hostname-pattern'
+import { ensureFile } from 'extra-filesystem'
 
 export class HostnameList {
   private constructor(private patterns: HostnamePattern[]) {}
 
   static async create(filename: string): Promise<HostnameList> {
+    await ensureFile(filename)
     const patterns = await readHostnameListFile(filename)
 
     return new HostnameList(patterns)
