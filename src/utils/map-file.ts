c9da58040ca9ff5df0793db67aa2fba20877da71
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-07-31 13:15:33 +0800

    perf: reduce memory usage

--- src/utils/map-file.ts
+++ src/utils/map-file.ts
@@ -1,17 +1,22 @@
-import { IterableOperator } from 'iterable-operator/lib/es2018/style/chaining'
+import { IterableOperator, AsyncIterableOperator } from 'iterable-operator/lib/es2018/style/chaining'
 import { promises as fs } from 'fs'
-import { ensureFile } from 'extra-filesystem'
+import { ensureFile, readFileLineByLine } from 'extra-filesystem'
 
 export async function readMapFile<T, U>(
   filename: string
 ): Promise<Map<T, U>> {
   await ensureFile(filename)
-  const text = await fs.readFile(filename, 'utf-8')
-  const entries: Array<[key: T, value: U]> = text.split('\n')
-    .filter(x => !!x)
-    .map(x => JSON.parse(x))
 
-  return new Map(entries)
+  const map = new Map()
+  const iter = new AsyncIterableOperator(readFileLineByLine(filename))
+    .filterAsync(x => !!x)
+    .mapAsync(x => JSON.parse(x) as [key: T, value: U])
+
+  for await (const [key, value] of iter) {
+    map.set(key, value)
+  }
+
+  return map
 }
 
 export async function writeMapFile<T, U>(
